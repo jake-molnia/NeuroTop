1216919842697499d3dbb0d395cceab2119640c2
Jacob Molnia <jrmolnia@wpi.edu>
2025-07-12 16:23:33 -0400

    working code..

--- ntop/__init__.py
+++ ntop/__init__.py
@@ -1,13 +1,1 @@
-from .core.models import MLP, simple_mlp
-from .core.activations import extract_activations, ActivationHook
-from .core.training import Trainer
-from .core.monitoring import TopologyMonitor
-from .analysis.distances import compute_distances
-from .analysis.persistence import compute_persistence, PersistenceAnalyzer
-from .analysis.evolution import EvolutionTracker
-from .viz.plots import plot_persistence_diagram
-from .viz.temporal import plot_betti_evolution, create_training_summary
-from .data.mnist import get_mnist_loaders, create_simple_mnist_model
-from .data.fashion_mnist import get_fashion_mnist_loaders, create_simple_fashion_mnist_model
-
-__version__ = "0.1.0"
+__version__ = "0.0.0a1"
--- ntop/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import MLP, simple_mlp
-from .training import Trainer, quick_train
-from .activations import extract_activations, ActivationHook
